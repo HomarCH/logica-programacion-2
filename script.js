8c83435756bf0229934fa57c534055e13451147d
homar <necrokatze@gmail.com>
2026-08-26 17:59:21 -0600

    se agrega estilo

--- script.js
+++ script.js
@@ -17,7 +17,7 @@ function convertTemperature() {
     const fahrenheit = (celsius * 9 / 5) + 32;
     const kelvin = celsius + 273.15;
 
-    // Output results rounded to 2 decimal places
+    //mostrar los resultados
     fahrenheitField.value = fahrenheit.toFixed(2) + " °F";
     kelvinField.value = kelvin.toFixed(2) + " K";
 }
