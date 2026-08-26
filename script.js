573c04761fd21e61035017b049c5c5f8010cab06
homar <necrokatze@gmail.com>
2026-08-26 17:26:06 -0600

    se actualizan archivos

---
+++ script.js
@@ -0,0 +1,23 @@
+function convertTemperature() {
+    // obtener el valor de entrada y convertirlo a un número de floating-point
+    const celsiusInput = document.getElementById('celsius').value;
+    const fahrenheitField = document.getElementById('fahrenheit');
+    const kelvinField = document.getElementById('kelvin');
+
+    // Si el input está vacío, limpiar los campos de salida y salir de la función
+    if (celsiusInput === "") {
+        fahrenheitField.value = "";
+        kelvinField.value = "";
+        return;
+    }
+
+    const celsius = parseFloat(celsiusInput);
+
+    //Aplicación de las fórmulas de conversión
+    const fahrenheit = (celsius * 9 / 5) + 32;
+    const kelvin = celsius + 273.15;
+
+    // Output results rounded to 2 decimal places
+    fahrenheitField.value = fahrenheit.toFixed(2) + " °F";
+    kelvinField.value = kelvin.toFixed(2) + " K";
+}
